9c735a4e6f758d58f56855b644b604bffc67338f
Petros Antoniou <peteranti@me.com>
2018-10-22 17:30:59 +0100

    Lesson 01

---
+++ coderswag/View/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  coderswag
+//
+//  Created by Petros Antoniou on 22/10/2018.
+//  Copyright © 2018 Petros Antoniou. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
